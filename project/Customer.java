9f4f1d15b85e7d813adab1619b72e839a1d78a8d
Lucaslimera <lucaslimacvel@hotmail.com>
2024-11-26 20:26:45 -0300

    Commit 8

--- project/Customer.java
+++ project/Customer.java
@@ -1,60 +1,77 @@
 package project;
 
-import java.util.Enumeration;
-import java.util.Vector;
-import java.util.Enumeration;
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Customer {
-   private String _name;
-   private Vector _rentals = new Vector();
+   private final String name;
+   private final List<Rental> rentals; // Lista de aluguéis
 
    public Customer(String name) {
-      _name = name;
+      this.name = name;
+      this.rentals = new ArrayList<>();
    }
 
-   public void addRental(Rental arg) {
-      _rentals.addElement(arg);
+   public void addRental(Rental rental) {
+      rentals.add(rental);
    }
 
    public String getName() {
-      return _name;
+      return name;
    }
 
-public String statement() {
-   Enumeration rentals = _rentals.elements();
-   String result = "project.Rental Record for " + getName() + "\n";
-   while (rentals.hasMoreElements()) {
-      Rental each = (Rental) rentals.nextElement();
+   public String statement() {
+      StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
+      for (Rental rental : rentals) {
+         // Adiciona detalhes do aluguel
+         result.append("\t")
+                 .append(rental.getMovie().getTitle())
+                 .append("\t")
+                 .append(rental.getCharge())
+                 .append("\n");
+      }
 
-      // show figures for this rental
-      result += "\t" + each.getMovie().getTitle() + "\t" +
-              String.valueOf(each.getCharge()) + "\n";
+      // Adiciona o rodapé
+      result.append("Amount owed is ")
+              .append(getTotalCharge())
+              .append("\nYou earned ")
+              .append(getTotalFrequentRenterPoints())
+              .append(" frequent renter points");
+
+      return result.toString();
    }
 
-   // add footer lines
-   result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-   result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
-           " frequent renter points";
-   return result;
-}
+   /**
+    * Gera o relatório de locação em formato HTML.
+    */
+   public String htmlStatement() {
+      StringBuilder result = new StringBuilder("<H1>Rentals for <EM>")
+              .append(getName())
+              .append("</EM></H1><P>\n");
 
-private double getTotalCharge() {
-   double result = 0;
-   Enumeration rentals = _rentals.elements();
-   while (rentals.hasMoreElements()) {
-      Rental each = (Rental) rentals.nextElement();
-      result += each.getCharge();
-   }
-   return result;
-}
+      for (Rental rental : rentals) {
+         // Adiciona detalhes do aluguel em formato HTML
+         result.append(rental.getMovie().getTitle())
+                 .append(": ")
+                 .append(rental.getCharge())
+                 .append("<BR>\n");
+      }
 
-private int getTotalFrequentRenterPoints() {
-   int result = 0;
-   Enumeration rentals = _rentals.elements();
-   while (rentals.hasMoreElements()) {
-      Rental each = (Rental) rentals.nextElement();
-      result += each.getFrequentRenterPoints();
+      // Adiciona o rodapé em formato HTML
+      result.append("<P>You owe <EM>")
+              .append(getTotalCharge())
+              .append("</EM><P>\nOn this rental you earned <EM>")
+              .append(getTotalFrequentRenterPoints())
+              .append("</EM> frequent renter points<P>");
+
+      return result.toString();
    }
-   return result;
+
+   private double getTotalCharge() {
+      return rentals.stream().mapToDouble(Rental::getCharge).sum();
+   }
+
+   private int getTotalFrequentRenterPoints() {
+      return rentals.stream().mapToInt(Rental::getFrequentRenterPoints).sum();
+   }
 }
